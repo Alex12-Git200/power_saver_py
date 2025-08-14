19ae657ba0334960c6a749dc2f5358c19b414ba4
Alex12-Git200 <alexgkorntoulava@gmail.com>
2025-08-14 04:07:29 +0200

    Checking if user changes profile manually

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+
 import psutil
 import os
+import logging
 from time import sleep
+import subprocess
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s')
 
 command_str = None
 last_command_str = None
@@ -8,36 +13,32 @@ last_command_str = None
 battery = psutil.sensors_battery()
 
 if battery is None:
-    print("No battery found.")
+    logging.error("No battery found.")
     exit(1)
 
-
 while True:
-    battery = psutil.sensors_battery()
 
+    battery = psutil.sensors_battery()
     percent = battery.percent
     secs_left = battery.secsleft
     is_plugged = battery.power_plugged
 
     if is_plugged:
         command_str = "balanced"
-
     if is_plugged and percent >= 50:
         command_str = "performance"
-
     if not is_plugged and percent <= 35:
         command_str = "power-saver"
-
     if not is_plugged and percent >= 35:
         command_str = "balanced"
-
     if not is_plugged and percent >= 90:
         command_str = "performance"
 
+    current_profile = subprocess.check_output("powerprofilesctl get", shell = True, text = True).strip()
 
-    if command_str != last_command_str:
-        print(f"Setting mode to {command_str}")
+    if command_str != last_command_str or current_profile != command_str:
+        logging.info(f"Setting mode to {command_str}")
         os.system(f"powerprofilesctl set {command_str}")
         last_command_str = command_str
 
-    sleep(10)
+    sleep(5)
